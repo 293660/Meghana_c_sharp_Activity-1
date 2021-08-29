4571479981e7a1f13deb48785534fc1ec8801f0a
Grandhi Neela Meghana <80732481+293660@users.noreply.github.com>
2021-08-29 20:43:41 +0530

    Add files via upload

---
+++ Activity 3/lambda Expressions/linq array/linq array/lambda discards/lambda discards/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace lambda_discards
+{
+    class MyForm : Form
+    {
+        private FlowLayoutPanel flowPanel;
+
+        public MyForm()
+        {
+            InitComponents();
+        }
+
+        private void InitComponents()
+        {
+            Text = "Quit button";
+            ClientSize = new Size(800, 450);
+
+            flowPanel = new FlowLayoutPanel();
+
+            flowPanel.Dock = DockStyle.Fill;
+            flowPanel.BorderStyle = BorderStyle.FixedSingle;
+
+            var button = new Button();
+            button.Margin = new Padding(10, 10, 0, 0);
+
+            button.Text = "Quit";
+            button.AutoSize = true;
+            button.Click += (_, _) => Close();
+
+            flowPanel.Controls.Add(button);
+            Controls.Add(flowPanel);
+
+            CenterToScreen();
+        }
+
+        [STAThread]
+        static void Main()
+        {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.Run(new MyForm());
+        }
+    }
+}
