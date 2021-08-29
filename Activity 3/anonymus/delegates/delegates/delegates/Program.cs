8ebe35bc0121de5fc090780734591fbb5f71b917
Grandhi Neela Meghana <80732481+293660@users.noreply.github.com>
2021-08-29 22:26:30 +0530

    Add files via upload

---
+++ Activity 3/anonymus/delegates/delegates/delegates/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegates
+{
+    class Test
+    {
+        delegate void TestDelegate(string s);
+        static void M(string s)
+        {
+            Console.WriteLine(s);
+        }
+
+        static void Main(string[] args)
+        {
+            // Original delegate syntax required
+            // initialization with a named method.
+            TestDelegate testDelA = new TestDelegate(M);
+
+            // C# 2.0: A delegate can be initialized with
+            // inline code, called an "anonymous method." This
+            // method takes a string as an input parameter.
+            TestDelegate testDelB = delegate (string s) { Console.WriteLine(s); };
+
+            // C# 3.0. A delegate can be initialized with
+            // a lambda expression. The lambda also takes a string
+            // as an input parameter (x). The type of x is inferred by the compiler.
+            TestDelegate testDelC = (x) => { Console.WriteLine(x); };
+
+         
+            testDelA("Hello. My name is M and I write lines.");
+            testDelB("That's nothing. I'm anonymous and ");
+            testDelC("I'm a famous author.");
+
+           .
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
