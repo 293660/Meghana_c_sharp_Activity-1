ec375ebef322e12c2ea15a550833c07a0c465f7d
Grandhi Neela Meghana <80732481+293660@users.noreply.github.com>
2021-08-30 23:49:35 +0530

    Create Module6.cs

---
+++ fundamentals of c#/Module6.cs
@@ -0,0 +1,81 @@
+ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Activity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+ int firstNumber = 12;
+            int secondNumber = 7;
+            Console.WriteLine(firstNumber + secondNumber);
+            firstName = "Bob";
+            widgetsSold = 7;
+            Console.WriteLine(firstName + " sold " + widgetsSold + " widgets.");
+            firstName = "Bob";
+            widgetsSold = 7;
+            Console.WriteLine(firstName + " sold " + widgetsSold + 7 + " widgets.");
+            firstName = "Bob";
+            widgetsSold = 7;
+            Console.WriteLine(firstName + " sold " + (widgetsSold + 7) + " widgets.");
+            int sum = 7 + 5;
+            int difference = 7 - 5;
+            int product = 7 * 5;
+            int quotient = 7 / 5;
+            Console.WriteLine("Sum: " + sum);
+            Console.WriteLine("Difference: " + difference);
+            Console.WriteLine("Product: " + product);
+            Console.WriteLine("Quotient: " + quotient);
+            decimal decimalQuotient = 7.0m / 5;
+            Console.WriteLine("Decimal quotient: " + decimalQuotient);
+            decimalQuotient = 7 / 5.0m;
+            decimalQuotient = 7.0m / 5.0m;
+            decimalQuotient = 7 / 5.0m;
+            decimalQuotient = 7.0m / 5;
+            decimalQuotient = 7.0m / 5.0m;
+            decimalQuotient = 7 / 5;
+            int first = 7;
+            int second = 5;
+            decimal quotient1 = (decimal)first / (decimal)second;
+            Console.WriteLine(quotient1);
+            Console.WriteLine("Modulus of 200 / 5 : " + (200 % 5));
+            Console.WriteLine("Modulus of 7 / 5: " + (7 % 5));
+            int value1 = 3 + 4 * 5;
+            int value2 = (3 + 4) * 5;
+            Console.WriteLine(value1);
+            Console.WriteLine(value2);
+            int value11 = 0;
+            value11 = value1 + 5;
+            value11 += 5;
+            value11 = 0;
+            value11 = value1 + 1;
+            value11++;
+            value11 = 1;
+            value11 = value1 + 1;
+            Console.WriteLine("First increment: " + value11);
+            value11 += 1;
+            Console.WriteLine("Second increment: " + value11);
+            value11++;
+            Console.WriteLine("Third increment: " + value11);
+            value11 = value1 - 1;
+            Console.WriteLine("First decrement: " + value11);
+            value11 -= 1;
+            Console.WriteLine("Second decrement: " + value11);
+            value11--;
+            Console.WriteLine("Third decrement: " + value11);
+            value11++;
+            Console.WriteLine("First: " + value11);
+            Console.WriteLine("Second: " + value11++);
+            Console.WriteLine("Third: " + value11);
+            Console.WriteLine("Fourth: " + (++value11));
+            Console.WriteLine("Second: " + value11++);
+            Console.WriteLine("Third: " + value11);
+            int fahrenheit = 94;
+            decimal celsius = (fahrenheit - 32m) * (5m / 9m);
+            Console.WriteLine("The temperature is " + celsius + " Celsius.");
+}            
+            
