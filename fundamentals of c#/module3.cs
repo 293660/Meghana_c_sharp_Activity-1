70c5aec4bd0995d3cf3067f419967fa3518d4562
Grandhi Neela Meghana <80732481+293660@users.noreply.github.com>
2021-08-30 23:44:05 +0530

    Create module3.cs

---
+++ fundamentals of c#/module3.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Activity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine('b');
+            //Console.WriteLine('Hello World!');
+            Console.WriteLine(123);
+            Console.WriteLine(12.3m);
+            Console.WriteLine(true);
+            Console.WriteLine(false);
+            Console.WriteLine("123");
+            Console.WriteLine(123);
+            Console.WriteLine("true");
+            Console.WriteLine(true);
+            //char userOption;
+            //int gameScore;
+            //decimal particlesPerMillion;
+            //bool processedCustomer;
+            //int firstName1;
+            //firstName1 = "Bob";
+            firstName = "Bob";
+            Console.WriteLine(firstName);
+            firstName = "Beth";
+            Console.WriteLine(firstName);
+            firstName = "Conrad";
+            Console.WriteLine(firstName);
+            firstName = "Grant";
+            Console.WriteLine(firstName);
+            string value = "Bob";
+            Console.WriteLine(value);
+            //var message = "Hello world!";
+            //message = 10.0m;
+            //var message;
+            string name = "Bob";
+            int messages = 3;
+            decimal temperature = 34.4m;
+            Console.Write("Hello, ");
+            Console.Write(name);
+            Console.Write("! You have ");
+            Console.Write(messages);
+            Console.Write(" in your inbox. The temperature is ");
+            Console.Write(temperature);
+            Console.Write(" celsius.");
+}
