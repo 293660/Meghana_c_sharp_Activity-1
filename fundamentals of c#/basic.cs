df8df1f779182530c3015340993bc240e82d72ec
Grandhi Neela Meghana <80732481+293660@users.noreply.github.com>
2021-08-30 23:37:45 +0530

    Update basic.cs

--- fundamentals of c#/basic.cs
+++ fundamentals of c#/basic.cs
@@ -1,1 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace C_Sharp_Activity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // MODULE 1
+
+            Console.WriteLine("Hello World!");
+            //console.WriteLine("Hello World!");
+            //Console.WriteLine('Hello World!');
+            // Console.WriteLine("Hello World!");
+            Console.Write("Congratulations!");
+            Console.Write(" ");
+            Console.Write("You wrote your first lines of code!");
+            Console.WriteLine("This is the first line.");
+            Console.Write("This is ");
+            Console.Write("the second ");
+            Console.Write("line.");
